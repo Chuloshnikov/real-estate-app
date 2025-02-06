30b5f77dd059f8e8f6bb7254e42b6c23fa7edee8
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-02-06 18:02:13 +0200

    done

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const Explore = () => {
+  return (
+    <View>
+        <Text>
+            
+        </Text>
+    </View>
+  )
+}
+
+export default Explore;
